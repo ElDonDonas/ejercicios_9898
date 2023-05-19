fff2ee46d081322a30b3345d3bc192837157490d
Paul Jaramillo <pauljara14@gmail.com>
2023-05-19 10:06:05 -0500

    ejemplo de pilas en java

---
+++ Primer parcial/PilaExample.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class PilaExample {
+    public static void main(String[] args) {
+        Stack<Integer> pila = new Stack<>();
+        pila.push(10); // Insertar elemento 10 en la pila
+        pila.push(20); // Insertar elemento 20 en la pila
+        pila.push(30); // Insertar elemento 30 en la pila
+
+        while (!pila.empty()) {
+            System.out.print(pila.peek() + " "); // Imprimir elemento superior de la pila
+            pila.pop(); // Eliminar elemento superior de la pila
+        }
+    }
+}
