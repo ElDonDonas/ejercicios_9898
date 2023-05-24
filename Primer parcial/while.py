7b7ca30c44b54aac4a8bcc27e1d4fd83273f67d6
Paul Jaramillo <pauljara14@gmail.com>
2023-05-24 10:57:55 -0500

    ciclos repetitivos

---
+++ Primer parcial/while.py
@@ -0,0 +1,31 @@
+i = 0
+
+# while i <= 10:
+#     print(i)
+#     # i = i+1
+#     i+=1
+
+# #break
+
+# while i <= 10:
+#     print(i)
+#     # i = i+1
+#     if i ==5:
+#         break
+#     i+=1
+
+# while i <= 10:
+#     i = i+1
+#     if i==5:
+#         continue
+#     print(i)
+
+personas = ["juan", "pedro", "yurmom"]
+edades = [2, 27, 14]
+for persona in personas:
+    
+    for edad in edades:
+        print(persona, edad)
+        i += 1
+        break
+    
