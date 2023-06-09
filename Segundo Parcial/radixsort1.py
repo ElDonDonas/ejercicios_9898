da7ad9947af50e58b6ed4ae31f33e4a511a8f052
Paul Jaramillo <pauljara14@gmail.com>
2023-06-09 10:53:49 -0500

    radix logaritm

---
+++ Segundo Parcial/radixsort1.py
@@ -0,0 +1,41 @@
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+
+def radix_sort(arr):
+    max_num = max(arr)
+    exp = 1
+
+    while max_num // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+# Solicitar al usuario una lista de números
+numbers = input("Ingresa una lista de números separados por espacios: ").split()
+numbers = [int(num) for num in numbers]
+
+# Ordenar la lista utilizando el algoritmo de Ordenación por Radix
+radix_sort(numbers)
+
+# Imprimir la lista ordenada
+print("Lista ordenada:")
+print(numbers)
